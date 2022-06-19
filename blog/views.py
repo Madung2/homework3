394a76b9914e4192a739b9321b5584b76133e9ba
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-20 00:16:46 +0900

    5번

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 from blog.models import Article as ArticleModel
+from blog.models import Category as CategoryModel
 
-
+# path('article/',
 class ArticleView(APIView):
     # 로그인 한 사용자의 게시글 목록 return
     permission_classes = [permissions.IsAuthenticated]
@@ -21,4 +22,28 @@ class ArticleView(APIView):
             titles.append(article.title)
 
         return Response({"article_list": titles})
+
+    def post(self, request):
+        user= request.user
+        contents = request.data.get('contents','')
+        title = request.data.get('title','')
+        category = request.data.get('category', '')
+        print(len(contents))
+        print(len(category))
+        print(len(title))
+
+        if len(title) <6 or len(contents) <21:
+            return Response({"message":"게시글을 작성할 수 없습니다"})
+        if not category:
+            return Response({"message":"카테고리를 지정해야 합니다"})
+        categories = [CategoryModel.objects.get(name=category)]
+        print(categories)
+        
     
+        my_article = ArticleModel.objects.create(user= user, contents=contents, title=title)
+        my_article.category.add(*categories)
+        my_article.save()
+
+
+        return Response({"message":"article"})
+
--- user/serializers.py
+++ user/serializers.py
@@ -3,6 +3,18 @@ from rest_framework import status
 from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel
 from user.models import Hobby as HobbyModel
+from blog.models import Article as ArticleModel
+from blog.models import Comment as CommentModel
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommentModel
+        fields = ["content"]
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleModel
+        fields = ["title", "contents"]
 
 class HobbySerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,9 +30,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     userprofile = UserProfileSerializer()
+    article = ArticleSerializer(many=True, source = "article_set")#역참조
+    comment = CommentSerializer(many=True, source = "comment_set")
+
 
     class Meta:
         model=UserModel
-        fields = ["username", "fullname", "join_date", "userprofile"]
+        fields = ["username", "fullname", "join_date", "userprofile", "article","comment"]
+
 
 
